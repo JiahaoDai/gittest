61e1bf537cc61fefd57fb3c82f5b6a68c3e9b51a
jiahaodai <418314316@qq.com>
2022-03-25 20:32:38 +0800

    hot-fix version2

--- src/main/java/controller/hello.java
+++ src/main/java/controller/hello.java
@@ -6,6 +6,6 @@ public class hello {
         System.out.println("hellp2");
         System.out.println("hellp3");
         System.out.println("hellp4");
-        System.out.println("hot-fix commit1");
+        System.out.println("hot fix 2");
     }
 }
