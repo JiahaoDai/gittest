7a7fef466a88ef6f37d2782f8f50d80aede8ce00
jiahaodai <418314316@qq.com>
2022-03-25 20:07:08 +0800

    third version

--- src/main/java/controller/hello.java
+++ src/main/java/controller/hello.java
@@ -3,5 +3,7 @@ package controller;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hellp");
+        System.out.println("hellp2");
+        System.out.println("hellp3");
     }
 }
